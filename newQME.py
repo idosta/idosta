f854020efa9d3195b0db9766dcc0f6fc4c7833f9
Ido <idozemach@icloud.com>
2021-12-08 13:57:42 +0200

    final

--- newQME.py
+++ newQME.py
@@ -2,106 +2,131 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.linalg import expm
 import pandas as pd
+from scipy.linalg import eig
+from pylab import *
 
 
-def QME(tl, v, epsilon, beta, U, ga):
+def QME(tl ,tm, v, epsilon, beta, U):
     FI = []
-    for a in [0,1]:
-        tm = np.sqrt(tl * ga)
-        miu = (v * ga / 2, -v * ga / 2)
-        U = U * ga
-        beta = beta / ga
-        eps_m = -U / 2 + epsilon * ga
-        # E is the energy of the dot=(00,up0,down0,updown)
-        E = (0, eps_m, eps_m, 2 * eps_m + U)
-        n = (0, 1, 1, 2)
-        elc = np.zeros((4, 4))
-        for i in range(4):
-            for j in range(4):
-                elc[i, j] = (n[i] - n[j])
-                if abs(elc[i, j]) > 1.1:
-                    elc[i, j] = 0
+
+    #phisical parameters
+
+    ga=tm**2/tl
+    miu = (v * ga / 2, -v * ga / 2)
+    U = U * ga
+    beta = beta / ga
+    eps_m = -U / 2 + epsilon * ga
+    # E is the energy of the dot=(00,up0,down0,updown)
+    E = (0, eps_m, eps_m, 2 * eps_m + U)
+
+    n = (0, 1, 1, 2)
+    elc = np.zeros((4, 4))
+    for i in range(4):
+        for j in range(4):
+            elc[i, j] = (n[i] - n[j])
+            if abs(elc[i, j]) > 1.1:
+                elc[i, j] = 0
+    #numerical parameters
+
+    dt = 1
+    tmax = 50
+    ddt=0.01
+
+    #initial conditioin
+    p0 = np.zeros(4)
+    for i in range(4):
+        p0[i] = np.exp(-beta * E[i])
+    p0 = p0 / sum(p0)
+
+    for a in [0, 1]:
 
         def gamma(tb, tm, eps_l, w):
-            temp = 0
             if abs(w - eps_l) < (2 * tb):
-                temp = ((tm ** 2) / (2 * tb ** 2)) * np.sqrt(4 * tb ** 2 - (w - eps_l) ** 2)
-            return temp
-
-        def f(miu, beta, w):
-            if w > 0:
-                return 1 / (np.exp(beta * (w - miu)) + 1)
+                return ((tm ** 2) / (2 * tb ** 2)) * np.sqrt(4 * tb ** 2 - (w - eps_l) ** 2)
             else:
-                return 1 - (1 / (np.exp(beta * (-w - miu)) + 1))
+                return 0
 
-        R = [np.zeros((4, 4)), np.zeros((4, 4))]
-        for l in range(2):
+        def fermi(beta, mu, e):
+            return 1.0 / (np.exp(beta * (e - mu)) + 1)
+
+        def getR(beta, mu, E):
+
+            def gu(n, m):
+                return gamma(tl, tm, mu, E[m] - E[n]) * fermi(beta, mu, E[n] - E[m])
+
+            def gd(n, m):
+                return gamma(tl, tm, mu, E[n] - E[m]) * (1.0 - fermi(beta, mu, E[m] - E[n]))
+
+            R = array( \
+                [array([0., gu(1, 0), gu(2, 0), 0.]),
+                 array([gd(0, 1), 0., 0., gu(3, 1)]),
+                 array([gd(0, 2), 0., 0., gu(3, 2)]),
+                 array([0., gd(1, 3), gd(2, 3), 0.]),
+                 ])
+            return R
+
+        RL, RR = getR(beta, miu[0], E), getR(beta, miu[1], E)
+        R = RL + RR
+
+        # Generate M matrix:
+        def get_M(lamb,a):
+            M = zeros((4, 4))
             for i in range(4):
                 for j in range(4):
-                    R[l][i, j] = abs(elc[i, j]) * gamma(tl, tm, miu[l], elc[i, j] * (E[i] - E[j])) * f(miu[l], beta,
-                                                                                                       (E[i] - E[j]))
+                    if i == j:
+                        M[i, j] = sum(R[i, :])
+                    else:
+                        if a==0:
+                            M[i, j] = -RR[j, i] - RL[j, i] * exp(lamb * elc[i, j])
+                        if a==1:
+                            M[i, j] = -RR[j, i] * exp(-lamb * elc[i, j]) - RL[j, i]
+            return M
 
-        m = np.copy(R)
-        for l in range(2):
-            for i in range(4):
-                m[l][i, i] = -(m[l][0, i] + m[l][1, i] + m[l][2, i] + m[l][3, i])
+#        def p(t, lamb, a, p0):
+#            return np.matmul(expm(-get_M(lamb, a) * t), p0)
 
-        p0 = np.zeros(4)
-        for i in range(4):
-            p0[i] = np.exp(-beta * E[i])
-        p0 = p0 / sum(p0)
+        def pdt(tm, lamb, a, p0):
+            M = get_M(lamb, a)
+            P = p0.copy()
+            Ps = [p0]
+            times = arange(0.0, tm, ddt)
 
-        def modmatrix(elc, x):
-            q = np.zeros((4, 4))
-            for i in range(4):
-                for j in range(4):
-                    q[i, j] = np.exp(x * (elc[i, j]))  # can count total
-            return q
+            for t in times[1:]:
+                oldP = P.copy()
+                for i in range(4):
+                    P[i] = oldP[i] - ddt * dot(M[i, :], oldP)
+                Ps.append(P.copy())
+            return Ps[len(Ps)-1]
 
-        def p(x, t, m, p0, a):
-            M = np.zeros((4, 4))
-            if a == 1:
-                M = (m[0] + m[1] * modmatrix(elc, x))
-            if a == 0:
-                M = (m[0] * modmatrix(elc, x) + m[1])
-
-            # a=0 for the right lead and a=1 for the left lead
-            return np.matmul(expm(M * t), p0)
-
-        def C_cal(t, m, p0, a):
+        def C_cal(t, p0, a):
             d = []
 
-            def f(x):
-                return np.log(sum(p(x, t, m, p0, a)))
+            def g(lamb):
+                return np.log(sum(pdt(t, lamb, a, p0)))
+            h = 0.001
+            return 0.5 * (g(h) - g(-h)) / h
 
-            h = 1
-            d.append(0.5 * (f(h) - f(-h)) / h)
-            h = h / 10
-            d.append(0.5 * (f(h) - f(-h)) / h)
-            while d[len(d) - 1] - d[len(d) - 2] > 0.001 * d[len(d) - 1]:
-                h = h / 10
-                d.append(0.5 * (f(h) - f(-h)) / h)
-            return d[len(d) - 1]
+        t = [tmax]
+        C = [0]
+        I = [0]
 
-        t = []
-        t.append(0)
-        C = []
-        C.append(0)
-        I = []
-        I.append(0)
-        dt = 1
-        for i in range(10000):
-            t.append(t[len(t) - 1] + dt)
-            C.append(C_cal(t[len(t) - 1], m, p0, 0))
-            I.append(C[len(C) - 1] / t[len(t) - 1])
-        if I[len(I) - 1] > 0:
-            while (I[len(C) - 1] - I[len(C) - 2]) > 0.001 * (I[len(C) - 10] - I[len(C) - 11]):
-                t.append(t[len(t) - 1] + dt)
-                C.append(C_cal(t[len(t) - 1], m, p0, 0))
-                I.append(C[len(C) - 1] / t[len(t) - 1])
+        t.append(t[len(t) - 1] + dt)
+        C.append(C_cal(t[len(t) - 1], p0, a))
+        I.append(C[len(C) - 1] / t[len(t) - 1])
+        t.append(t[len(t) - 1] + 1 + dt)
+        C.append(C_cal(t[len(t) - 1], p0, a))
+        I.append(C[len(C) - 1] / t[len(t) - 1])
+
 
         FI.append(I[len(C) - 1])
-    return FI[0], FI[1]
+
+    def get_current(p, m):
+        return p[0] * (m[0, 1] + m[0, 2]) + \
+               p[1] * (- m[1, 0] + m[1, 3]) + \
+               p[2] * (- m[2, 0] + m[2, 3]) + \
+               p[3] * (- m[3, 2] - m[3, 1])
+
+    return FI[0], FI[1], get_current(pdt(tmax, 0, 0, p0), RL), get_current(pdt(tmax, 0, 1, p0), -RR)
 
 
 data = []
@@ -109,15 +134,15 @@ beta = 1
 U = 40
 tlrange = [25]
 T = 0
-ga = 1
-for v in range(-20, 120, 20):
-    for epsilon in range(-100, 120, 20):
+tm = 5
+for v in range(-100, 101, 1):
+    for epsilon in range(-100, 105, 5):
         for tl in tlrange:
-            I = QME(tl, v, epsilon, beta, U, ga)
-            data.append([tl, v, epsilon, beta, U, ga, I[0], I[1]])
+            I = QME(tl, tm, v, epsilon, beta, U)
+            data.append([tl, tm, v, epsilon, beta, U, I[0], I[1], I[2], I[3]])
             print(I)
             T = T + 1
             print(T)
 
-df = pd.DataFrame(data, columns=["tl", "v", "epsilon", "beta", "U", "gamma", "IR", "IL"])
-df.to_csv('01122021.csv')
+df = pd.DataFrame(data, columns=["tl" , "tm", "v", "epsilon", "beta", "U", "IL", "IR", "IL_DIRECT", "IR_DIRECT"])
+df.to_csv('08122021hr.csv')
